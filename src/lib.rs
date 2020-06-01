b088e96462f4b96c76b3520b2932a0d3c6106250
Maximilian Goisser <goisser94@gmail.com>
2020-06-01 19:28:39 +0200

    Add support for upcoming 2.0.0 features

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub mod prelude {
-    pub use crate::{print_tooltips, TipLine, TipTextLine, TipUrlLine};
+    pub use crate::{print_tooltips, TipExecuteLine, TipInput, TipLine, TipTextLine, TipUrlLine};
 }
 
 use serde::{Deserialize, Serialize};
@@ -10,6 +10,7 @@ use serde::{Deserialize, Serialize};
 pub enum TipLine {
     Text(TipTextLine),
     Url(TipUrlLine),
+    Execute(TipExecuteLine),
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -32,6 +33,53 @@ pub struct TipUrlLine {
 impl Into<TipLine> for TipUrlLine {
     fn into(self) -> TipLine {
         TipLine::Url(self)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TipExecuteLine {
+    pub label: String,
+    pub args: Vec<String>,
+}
+
+impl Into<TipLine> for TipExecuteLine {
+    fn into(self) -> TipLine {
+        TipLine::Execute(self)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct TipInput {
+    pub args: Vec<String>,
+    pub bundle_identifier: Option<String>,
+}
+
+impl TipInput {
+    pub fn from_args() -> Self {
+        let mut main_args = Vec::new();
+        let mut bundle_identifier = None;
+
+        let mut previous_arg_was_bundleidentifier = false;
+        let mut args = std::env::args();
+        args.next().unwrap();
+        for arg in args {
+            if arg == "--bundle-identifier" {
+                previous_arg_was_bundleidentifier = true;
+                continue;
+            }
+            if previous_arg_was_bundleidentifier {
+                previous_arg_was_bundleidentifier = false;
+                bundle_identifier = Some(arg);
+                continue;
+            }
+
+            main_args.push(arg);
+        }
+
+        TipInput {
+            args: main_args,
+            bundle_identifier,
+        }
     }
 }
 
